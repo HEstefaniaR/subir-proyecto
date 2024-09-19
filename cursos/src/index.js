bdd0f96b526db5468bebe7adc35d23763d182ab7
HEstefaniaR <161893097+HEstefaniaR@users.noreply.github.com>
2024-09-19 14:08:05 -0500

    cambios

--- cursos/src/index.js
+++ cursos/src/index.js
@@ -3,10 +3,19 @@ const express = require('express');
 const cursosController = require('./controllers/cursosController');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+app.use(cors({
+    origin: 'http://localhost:3000, http://192.168.100.2'  // Permite solicitudes solo desde localhost:3000
+}));
+app.use(cors({
+    origin: '*',  // Permite solicitudes desde cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+    allowedHeaders: ['Content-Type']  // Encabezados permitidos
+}));
 
 app.use(cursosController); // Asegúrate de usar un prefijo para evitar conflictos con otras rutas
 
--- usuarios/src/index.js
+++ usuarios/src/index.js
@@ -7,9 +7,17 @@ app.use(morgan('dev'));
 app.use(express.json());
 const cors = require('cors');
 
+app.use(cors({
+    origin: 'http://localhost:3000, http://192.168.100.2'  // Permite solicitudes solo desde localhost:3000
+}));
+app.use(cors({
+    origin: '*',  // Permite solicitudes desde cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos permitidos
+    allowedHeaders: ['Content-Type']  // Encabezados permitidos
+}));
+
 app.use(estudiantesController);
 app.use(profesoresController);
-app.use(cors()); 
 
 app.listen(3005, () => { //El puerto del microservicio es 3005, cada microservicio se lanza en un puerto distinto
     console.log('Microservicio Usuarios ejecutandose en el puerto 3005');
